0fa63fdd3b161cced13ea1c349dadf1d9cef6b55
Shrivu Shankar <shrivu1122@gmail.com>
2020-09-28 13:57:10 -0500

    added people

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,6 +1,44 @@
 import React, { Component } from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+
+const GROUP_MEMBERS = [
+  {
+    name: "Shrivu Shankar",
+    gitlab: "sshh12",
+    role: "Full Stack Developer",
+    bio: "Junior Computer Science Major from Houston, TX",
+    unitTests: 0,
+  },
+  {
+    name: "Cameron Doggett",
+    gitlab: "camd99",
+    role: "Back-end Developer",
+    bio: "Junior Computer Science Major from Katy, TX",
+    unitTests: 0,
+  },
+  {
+    name: "Yash Kakodkar",
+    gitlab: "YashKakodkar",
+    role: "Full Stack Developer",
+    bio: "Junior Computer Science Major from Katy, TX",
+    unitTests: 0,
+  },
+  {
+    name: "Jeffrey Moulckers",
+    gitlab: "jrmoulckers",
+    role: "Full Stack Developer",
+    bio: "Junior Computer Science/Spanish Dual Major from Austin, TX",
+    unitTests: 0,
+  },
+  {
+    name: "Vassi Gianitsos",
+    gitlab: "vassi_gianitsos",
+    role: "Front-end Developer",
+    bio: "Senior Computer Science major from Flower Mound, TX",
+    unitTests: 0,
+  },
+].sort((a, b) => Math.random() - 0.5);
 
 export default class About extends Component {
   render() {
@@ -14,6 +52,22 @@ export default class About extends Component {
             </Button>
           </LinkContainer>
         </header>
+        <div>
+          {GROUP_MEMBERS.map((person) => (
+            <Card style={{ width: "18rem", display: "inline-block" }}>
+              <Card.Img variant="top" src={`/imgs/${person.gitlab}.jpg`} />
+              <Card.Body>
+                <Card.Title>{person.name}</Card.Title>
+                <Card.Text>
+                  {person.bio}
+                  <hr />
+                  <b>Role</b> {person.role}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
       </div>
     );
   }

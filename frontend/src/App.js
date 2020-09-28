ee807732ce2ecc0cf3c87dd1bd87cc435cdc7be3
Shrivu Shankar <shrivu1122@gmail.com>
2020-09-28 11:42:34 -0500

    rm extra css import, auto formatted a bit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,46 @@
-import React, { Component, Fragment } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
-import Routes from './routes'
+import React, { Component, Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Routes from "./routes";
 
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar bg='light' expand='lg'>
-          <LinkContainer to='/'><Navbar.Brand>COVID-19 DB</Navbar.Brand></LinkContainer>
-          
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='navbar'>
-              <LinkContainer to='/home'><Nav.Link>Home</Nav.Link></LinkContainer>
-              <LinkContainer to='/about'><Nav.Link>About</Nav.Link></LinkContainer>
-              <LinkContainer to='/countries'><Nav.Link>Countries</Nav.Link></LinkContainer>
-              <LinkContainer to='/cases'><Nav.Link>Cases</Nav.Link></LinkContainer>
-              <LinkContainer to='/risks'><Nav.Link>Risks</Nav.Link></LinkContainer>
+        <Navbar bg="light" expand="lg">
+          <LinkContainer to="/">
+            <Navbar.Brand>COVID-19 DB</Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="navbar">
+              <LinkContainer to="/home">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/countries">
+                <Nav.Link>Countries</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/cases">
+                <Nav.Link>Cases</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/risks">
+                <Nav.Link>Risks</Nav.Link>
+              </LinkContainer>
             </Nav>
             <Form inline>
-              <FormControl type='text' placeholder='Search not ready yet... :(' className='searchbar' />
-              <Button variant='outline-secondary'>Search</Button>
+              <FormControl
+                type="text"
+                placeholder="Search not ready yet... :("
+                className="searchbar"
+              />
+              <Button variant="outline-secondary">Search</Button>
             </Form>
           </Navbar.Collapse>
         </Navbar>
--- frontend/src/components/hoverDropdown.js
+++ frontend/src/components/hoverDropdown.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-import { NavDropdown } from 'react-bootstrap';
+import React, { Component } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../app.css";
 
 class HoverDropdown extends Component {
-    constructor() {
-        super()
-        this.state = { isOpen: false }
-    }
-  
-    handleOpen = () => {
-        this.setState({ isOpen: true })
-    }
-  
-    handleClose = () => {
-        this.setState({ isOpen: false })
-    }
-  
-    render() {
-        var component = (
-            <NavDropdown
-            title={this.props.title}
-            onMouseEnter = { this.handleOpen }
-            onMouseLeave = { this.handleClose }
-            show={ this.state.isOpen }
-            href={this.props.to}
-            >
-                {this.props.children}
-            </NavDropdown> 
-        );
+  constructor() {
+    super();
+    this.state = { isOpen: false };
+  }
 
-        // Conditionally add link if address is provided
-        if(this.props.to) {
-            component = <LinkContainer to={this.props.to} className={this.props.to}>{component}</LinkContainer>;
-        }
-        return component;
+  handleOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
+  handleClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  render() {
+    var component = (
+      <NavDropdown
+        title={this.props.title}
+        onMouseEnter={this.handleOpen}
+        onMouseLeave={this.handleClose}
+        show={this.state.isOpen}
+        href={this.props.to}
+      >
+        {this.props.children}
+      </NavDropdown>
+    );
+
+    // Conditionally add link if address is provided
+    if (this.props.to) {
+      component = (
+        <LinkContainer to={this.props.to} className={this.props.to}>
+          {component}
+        </LinkContainer>
+      );
     }
+    return component;
+  }
 }
 
 export default HoverDropdown;

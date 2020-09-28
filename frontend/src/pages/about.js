0f4b8b42b46accd4bc80907c76cb57b4f58a41c8
Shrivu Shankar <shrivu1122@gmail.com>
2020-09-28 14:40:41 -0500

    added tools

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Media, Tab, Row, Col, ListGroup } from "react-bootstrap";
 
 const REPO_ID = "21269899";
+
 const GROUP_MEMBERS = [
   {
     name: "Shrivu Shankar",
@@ -41,6 +42,26 @@ const GROUP_MEMBERS = [
   },
 ].sort((a, b) => Math.random() - 0.5);
 
+const TOOLS = [
+  {
+    name: "React",
+    url: "https://reactjs.org/",
+    desc: "For creating interactive UIs in JSX.",
+  },
+  {
+    name: "Flask",
+    url: "https://flask.palletsprojects.com/en/1.1.x/",
+    desc: "For serving our API data in a RESTful way.",
+  },
+];
+
+const DATA_SOURCES = [
+  "https://api.covid19api.com/",
+  "https://restcountries.eu/",
+  "https://newsapi.org/",
+  "https://covid.ourworldindata.org/data/owid-covid-data.json",
+];
+
 export default class About extends Component {
   constructor(props) {
     super(props);
@@ -58,7 +79,10 @@ export default class About extends Component {
 
   render() {
     let { issues, commits } = this.state;
-    console.log(issues, commits);
+    let totalUnitTests = GROUP_MEMBERS.reduce(
+      (acc, person) => acc + person.unitTests,
+      0
+    );
     return (
       <div className="App">
         <header className="App-header" style={{ minHeight: "10rem" }}>
@@ -69,6 +93,23 @@ export default class About extends Component {
             </Button>
           </LinkContainer>
         </header>
+        <div style={{ marginTop: "18px", paddingBottom: "18px" }}>
+          description of the site, its purpose, its intended users and
+          explanation of the interesting result of integrating disparate data
+        </div>
+        <hr />
+        <ProjectInfo />
+        <hr />
+        <div style={{ marginTop: "18px", paddingBottom: "18px" }}>
+          <h4>GitLab Statistics</h4>
+          <b>Commits</b> {commits.length} | <b>Issues</b> {issues.length} |{" "}
+          <b>Unit Tests</b> {totalUnitTests}
+          <br />
+          <a href="https://gitlab.com/jrmoulckers/covid19db-net">
+            gitlab.com/jrmoulckers/covid19db-net
+          </a>
+        </div>
+        <hr />
         <div>
           {GROUP_MEMBERS.map((person) => {
             let personIssues = issues.filter((issue) =>
@@ -107,4 +148,55 @@ export default class About extends Component {
       </div>
     );
   }
+}
+
+function ProjectInfo() {
+  return (
+    <div style={{ margin: "18px" }}>
+      <h4>Project Info</h4>
+      <br />
+      <Tab.Container defaultActiveKey="#tools">
+        <Row>
+          <Col sm={4}>
+            <ListGroup>
+              <ListGroup.Item action href="#tools">
+                Tools
+              </ListGroup.Item>
+              <ListGroup.Item action href="#data">
+                Data
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col sm={8}>
+            <Tab.Content>
+              <Tab.Pane eventKey="#tools" style={{ textAlign: "left" }}>
+                <ul className="list-unstyled">
+                  {TOOLS.map((tool) => (
+                    <li>
+                      <a href={tool.url}>{tool.name}</a> - {tool.desc}
+                    </li>
+                  ))}
+                </ul>
+              </Tab.Pane>
+              <Tab.Pane eventKey="#data" style={{ textAlign: "left" }}>
+                For the first phase, we downloaded a few local copies of the
+                data provided from these sources and glued them into the
+                instances using either a script or by copy-paste. The COVID-19
+                API, RestCountries, and NewsAPI all provided APIs to access
+                their data, but the OWID dataset is only available in file
+                format (<code>.json</code> in this case).
+                <ul className="list-unstyled">
+                  {DATA_SOURCES.map((url) => (
+                    <li>
+                      <a href={url}>{url}</a>
+                    </li>
+                  ))}
+                </ul>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
+  );
 }

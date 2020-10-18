4b8c0d090898824e8a89be9f7ca73dff9133930d
Jeffrey Moulckers <jrmoulckers@gmail.com>
2020-10-18 04:02:02 -0500

    defined risk factor statistics model and db interaction for state of interim db

--- backend/src/models.py
+++ backend/src/models.py
@@ -410,6 +410,116 @@ class RiskFactorStatistics(db.Model):
     population_density = db.Column(db.Float, nullable=True)
     # sources
     sources = db.Column(db.ARRAY(db.JSON), nullable=False)
+    
+    @staticmethod
+    def fix_attributes(attributes):
+        """
+        Translates the given request attributes into the corresponding columns in
+        the database.
+        """
+        attr_to_cols = {
+            "aged65Older": [RiskFactorStatistics.aged_65_older],
+            "aged70Older": [RiskFactorStatistics.aged_70_older],
+            "cardiovascDeathRate": [RiskFactorStatistics.cardiovascular_death_rate],
+            "country": [RiskFactorStatistics.country_alpha3_code],
+            "diabetesPrevalence": [RiskFactorStatistics.diabetes_prevalence],
+            "extremePovertyRate": [RiskFactorStatistics.extreme_poverty_rate],
+            "femaleSmokers": [RiskFactorStatistics.female_smokers],
+            "gdpPerCapita": [RiskFactorStatistics.gdp_per_capita],
+            "gini": [RiskFactorStatistics.gini],
+            "handwashingFacilities": [RiskFactorStatistics.handwashing_facilities],
+            "hospitalBedsPerThousand": [RiskFactorStatistics.hospital_beds_per_thousand],
+            "humanDevelopmentIndex": [RiskFactorStatistics.human_development_index],
+            "lifeExpectancy": [RiskFactorStatistics.life_expectancy],
+            "location": [],
+            "maleSmokers": [RiskFactorStatistics.male_smokers],
+            "medianAge": [RiskFactorStatistics.median_age],
+            "populationDensity": [RiskFactorStatistics.population_density],
+            "sources": [RiskFactorStatistics.sources],
+        }
+        fixed = list()
+        for attribute in attributes:
+            fixed += attr_to_cols[attribute]
+        return fixed
+
+    @staticmethod
+    def retrieve_all(attributes):
+        """
+        Retrieves all entries from the Risk Factor Statistics table in the database, including only the given attributes.
+        """
+        ret = []
+        all_entries = RiskFactorStatistics.query.options(load_only(*RiskFactorStatistics.fix_attributes(attributes))).all()
+        for entry in all_entries:
+            ret += [entry.polished(attributes)]
+        return ret
+
+    @staticmethod
+    def retrieve_by_id(identifier, id_type, attributes):
+        """
+        Retrieves the entry from the Risk Factor Statistics table in the database corresponding
+        to the given identifier. id_type should be one of const.Identifier. Only the fields indicated
+        by attributes are included in the retrieval.
+        """
+        entry = None
+        fixed_attr = RiskFactorStatistics.fix_attributes(attributes)
+        if id_type == const.Identifier.ALPHA3_CODE:
+            entry = RiskFactorStatistics.query.filter_by(country_alpha3_code=identifier).options(load_only(*fixed_attr)).first()
+        if entry is not None:
+            entry = entry.polished(attributes)
+        return entry
+
+    def polished(self, attributes):
+        """
+        Transforms this Countries database entry into a usable dict with only the given attributes.
+        """
+        ret = dict()
+        if "aged65Older" in attributes:
+            ret["aged65Older"] = self.aged_65_older
+        if "aged70Older" in attributes:
+            ret["aged70Older"] = self.aged_70_older
+        if "cardiovascDeathRate" in attributes:
+            ret["cardiovascDeathRate"] = self.cardiovascular_death_rate
+        if "country" in attributes:
+            ret["country"] = dict()
+            country = ret["country"]
+            country["name"] = None
+            country["codes"] = dict()
+            country["codes"]["alpha2Code"] = None
+            country["codes"]["alpha3Code"] = self.country_alpha3_code
+        if "diabetesPrevalence" in attributes:
+            ret["diabetesPrevalence"] = self.diabetes_prevalence
+        if "extremePovertyRate" in attributes:
+            ret["extremePovertyRate"] = self.extreme_poverty_rate
+        if "femaleSmokers" in attributes:
+            ret["femaleSmokers"] = self.female_smokers
+        if "gdpPerCapita" in attributes:
+            ret["gdpPerCapita"] = self.gdp_per_capita
+        if "gini" in attributes:
+            ret["gini"] = self.gini
+        if "handwashingFacilities" in attributes:
+            ret["handwashingFacilities"] = self.handwashing_facilities
+        if "hospitalBedsPerThousand" in attributes:
+            ret["hospitalBedsPerThousand"] = self.hospital_beds_per_thousand
+        if "humanDevelopmentIndex" in attributes:
+            ret["humanDevelopmentIndex"] = self.human_development_index
+        if "lifeExpectancy" in attributes:
+            ret["lifeExpectancy"] = self.life_expectancy
+        if "location" in attributes:
+            ret["location"] = dict()
+            ret["location"]["lat"] = None
+            ret["location"]["lng"] = None
+        if "maleSmokers" in attributes:
+            ret["maleSmokers"] = self.male_smokers
+        if "medianAge" in attributes:
+            ret["medianAge"] = self.median_age
+        if "populationDensity" in attributes:
+            ret["populationDensity"] = self.population_density
+        if "sources" in attributes:
+            ret["sources"] = self.sources
+        return ret
+
+    def __repr__(self):
+        return self.country_alpha3_code + " " + str(self.gini) + " " + str(self.gdp_per_capita)
 
     def __repr__(self):
         return self.country_alpha3_code + " " + str(self.gdp_per_capita) + " " + str(self.aged_70_older)

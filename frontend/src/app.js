d716b20ebeca54b70ed99cf428dee168828e256b
Vassi Gianitsos <vgianitsos@yahoo.com>
2020-10-17 19:48:43 -0500

    nav bar uses antd now instead of bootstrap

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,53 +1,62 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 import Routes from "./routes";
+import { Menu, Form, Input, Button } from 'antd';
 
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <Fragment>
-        <Navbar bg="light" expand="lg">
-          <LinkContainer to="/">
-            <Navbar.Brand>COVID-19 DB</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="navbar">
-              <LinkContainer to="/home">
-                <Nav.Link>Home</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/about">
-                <Nav.Link>About</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/countries">
-                <Nav.Link>Countries</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/cases">
-                <Nav.Link>Cases</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/risks">
-                <Nav.Link>Risks</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/globalNews">
-                <Nav.Link>Global News</Nav.Link>
-              </LinkContainer>
-            </Nav>
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search not ready yet... :("
-                className="searchbar"
-              />
-              <Button variant="outline-secondary">Search</Button>
+        <Menu mode="horizontal">
+          <Menu.Item>
+            <a href="/" rel="noopener noreferrer" style={{fontWeight: "bold", fontSize: 20}}>
+              COVID-19 DB
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="/home" rel="noopener noreferrer">
+              Home
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="/about" rel="noopener noreferrer">
+              About
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="/countries" rel="noopener noreferrer">
+              Countries
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="/cases" rel="noopener noreferrer">
+              Cases
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="/risks" rel="noopener noreferrer">
+              Risks
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="globalNews" rel="noopener noreferrer">
+              Global News
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            {/* TODO search bar here */}
+            <Form>
+              <Form.Item style={{ marginTop:'25px'}}>
+                <Input placeholder="Search bar not ready yet ... :(" />
+                <Button type="primary">Search</Button>                 
+              </Form.Item>                
             </Form>
-          </Navbar.Collapse>
-        </Navbar>
+          </Menu.Item>  
+        </Menu>
         <Routes />
       </Fragment>
+      
     );
   }
 }

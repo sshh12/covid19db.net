ff2e7a66550e6390c84c7015f8e30ff0a79a524d
Vassi Gianitsos <vgianitsos@yahoo.com>
2020-10-18 14:04:23 -0500

    switching from mocha to jest

--- frontend/tests.js
+++ frontend/tests.js
@@ -1,17 +1,4 @@
 /** 
- * Unit tests using Mocha.
- * To run tests: $ yarn mocha tests.js
+ * Unit tests using jest
  * 
  * */ 
-var assert = require('chai').assert;
-var sample = require('./sample');
-
-describe ('sample', function() {
-  it("sample should return running a test!", function() {
-    assert.equal(sample(), "running a test!");
-  });
-
-  it("sample should return String for type!", function() {
-    assert.typeOf(sample(), 'String');
-  });
-});

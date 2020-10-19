46b67142fd0afbc4d52ccef693c3730803acc161
Jeffrey Moulckers <jrmoulckers@gmail.com>
2020-10-19 03:31:03 -0500

    created country card to represent grid view in countries model page

--- frontend/src/components/country/countryCard.js
+++ frontend/src/components/country/countryCard.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
-import { Button, Card} from "antd";
+import React, { Component, Fragment } from "react";
+import { Card, Col, Row, Layout} from "antd";
 import { LinkContainer } from "react-router-bootstrap";
-import axios from 'axios';
+import { Link } from "react-router-dom";
 
-import "../countryInstances/countryInstance.css";
+import "./countryInstance.css";
 
 const { Meta } = Card;
+const { Content } = Layout;
 
 export default class CountryCard extends Component {
     allLanguages(myList) {
@@ -44,27 +45,40 @@ export default class CountryCard extends Component {
         );
     }
       
-    render() {
-        const {capital, codes, flag, languages, name, population } = this.props.data;
+    render() {  
+      const {capital, codes, flag, languages, name, population } = this.props.data;
         return (
-            <LinkContainer to={`/countries/${codes.alpha3Code}`}>
+          <Layout style={{ height: 360, width: 300, border: "1px grey"}}>
+            <Content style={{height: 310}}>
+              <Link to={`/countries/${codes.alpha3Code}`}>
                 <Card
-                extra={<this.CardExtra code={codes.alpha3Code} />}
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src={flag} />}
+                  hoverable
+                  cover={<img alt="example" src={flag} style={{ height: 165, width: 300}}/>}
+                  style={{ height: 310 }}
                 >
                 <Meta
-                    title={name}
-                    description={this.format(
+                  title={name}
+                  description={this.format(
                     codes.alpha3Code,
                     this.allLanguages(languages),
                     population,
                     capital.name
-                    )}
+                  )}
                 />
                 </Card>
-            </LinkContainer>            
+              </Link>              
+            </Content>
+            <Content style={{ height: 50, backgroundColor: "white" }}>
+              <Row>
+                <Col span={12}>
+                  <LinkContainer to={`/cases/${codes.alpha3Code}`}><a href="#">Cases</a></LinkContainer>
+                </Col>
+                <Col span={12}>                  
+                  <LinkContainer to={`/risks/${codes.alpha3Code}`}><a href="#">Risks</a></LinkContainer>
+                </Col>               
+              </Row>
+            </Content>
+          </Layout>
         )
     }
 }

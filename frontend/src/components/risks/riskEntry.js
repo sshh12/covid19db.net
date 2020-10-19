54420753c628816364a41bb28409cdb83a1af10e
Vassi Gianitsos <vgianitsos@yahoo.com>
2020-10-19 01:23:05 -0500

    dynamic risks page

--- frontend/src/components/risks/riskEntry.js
+++ frontend/src/components/risks/riskEntry.js
@@ -6,25 +6,108 @@ import axios from 'axios';
 
 export default class RiskEntry extends Component {
     render() {
-        const {capital, codes, flag, languages, name, population } = this.props.data;
+
+      const {country, location, populationDensity, humanDevelopmentIndex, gini, gdpPerCapita, medianAge, aged65Older, aged70Older, extremePovertyRate, cardiovascDeathRate, diabetesPrevalence, femaleSmokers, maleSmokers, hospitalBedsPerThousand, lifeExpectancy, handwashingFacilities} = this.props.data;
+      // const {country, location, populationDensity, humanDevelopmentIndex, gini} = this.props.data;
+    //   const columns = [
+    //   {
+    //     title: "Country",
+    //     dataIndex: "country",
+    //     key: "country",
+    //     render: (country) => (
+    //     <a href={`/countries/${country.codes.alpha3Code}`}>
+    //       {country.name}
+    //     </a>
+    //     ),
+    //     sorter: (a, b) => a.country.name.localeCompare(b.country.name),
+    //   },
+    //   {
+    //     title: "Life Expectancy",
+    //     dataIndex: "lifeExpectancy",
+    //     key: "lifeExpectancy",
+    //     sorter: (a, b) => a.lifeExpectancy - b.lifeExpectancy,
+    //   },
+    //   {
+    //     title: "Human Development Index",
+    //     dataIndex: "humanDevelopmentIndex",
+    //     key: "humanDevelopmentIndex",
+    //     sorter: (a, b) => a.humanDevelopmentIndex - b.humanDevelopmentIndex,
+    //   },
+    //   {
+    //     title: "Population Density",
+    //     dataIndex: "populationDensity",
+    //     key: "populationDensity",
+    //     render: (population) => <>{population.toLocaleString()}</>,
+    //     sorter: (a, b) => a.populationDensity - b.populationDensity,
+    //   },
+    //   {
+    //     title: "Gini",
+    //     dataIndex: "gini",
+    //     key: "gini",
+    //     sorter: (a, b) => a.gini - b.gini,
+    //   },
+    //   {
+    //     title: "Explore Risks",
+    //     dataIndex: "exploreRisk",
+    //     key: "exploreRisk",
+    //     render: (code) => (
+    //       <a href={`/risks/${code}`}>
+    //         <Button>Explore</Button>
+    //       </a>
+    //     ),
+    //   },
+    //   {
+    //     title: "Explore Cases",
+    //     dataIndex: "exploreRisk",
+    //     key: "exploreCase",
+    //     render: (code) => (
+    //       <a href={`/cases/${code}`}>
+    //         <Button>Explore</Button>
+    //       </a>
+    //     ),
+    //   },
+    // ];
+    
+    // const {country, location, populationDensity, humanDevelopmentIndex, gini, 
+    //   gdpPerCapita, medianAge, aged65Older, aged70Older, extremePovertyRate, 
+    //   cardiovascDeathRate, diabetesPrevalence, femaleSmokers, maleSmokers, 
+    //   hospitalBedsPerThousand, lifeExpectancy, handwashingFacilities}
+
+    const data = [
+      {
+        key: "1",
+        country: country.name,
+        lifeExpectancy: lifeExpectancy,
+        humanDevelopmentIndex: humanDevelopmentIndex,
+        populationDensity: populationDensity,
+        gini: gini,
+        exploreRisk: country.codes.alpha3Code,
+      }
+    ];
+
+
+
+
+
+        
         return (
           <div className="App">
             {/* <header className="App-header"> */}
-            <h1
-              style={{
-                fontWeight: "800",
-                fontSize: "2em",
-                marginTop: "20px",
-                marginBottom: "20px",
-              }}
+            <h1 style={{ fontWeight: "800", fontSize: "2em", marginTop: "20px", marginBottom: "20px" }}
             >
-              Risk Factors & Statistics{" "}
             </h1>
+            <div>
+            | country: {country.name} | Life Ex: {lifeExpectancy} | HDI: {humanDevelopmentIndex}
+              | Pop Density: {populationDensity} | GINI: {gini} | 
+            <a href={`/cases/${country.codes.alpha3Code}`}>
+              <Button>Explore</Button>
+            </a>
+            </div>
             {/* <Table
               style={{ margin: "0 5vw", outline: "1px solid lightgrey" }}
               columns={columns}
               dataSource={data}
-              pagination={false}
+              // pagination={false}
             /> */}
             {/* </header> */}
           </div>           
--- frontend/src/pages/risks.js
+++ frontend/src/pages/risks.js
@@ -1,17 +1,15 @@
 // import { LinkContainer } from "react-router-bootstrap";
 import React, { Component, Fragment } from "react";
-import { Button, Card, Col, Pagination, Row } from "antd";
+import { Button, Card, Col, Pagination, Row, Table} from "antd";
 // import { Button, Table, Tag, Space } from "antd";
 import axios from 'axios';
-import RiskEntry from "../components/risks/riskEntry";
-
-import USAData from "../components/risks/data/USA.json";
-import GBRData from "../components/risks/data/GBR.json";
-import MEXData from "../components/risks/data/MEX.json";
+// import RiskEntry from "../components/risks/riskEntry";
 
 export default class Risks extends Component {
+
+
   numPerPage = 15;
-  
+
   constructor() {
     super();
     this.state = {
@@ -23,11 +21,12 @@ export default class Risks extends Component {
   }
   // https://api.covid19db.net/
   // risk-factor-statistics
+  // 'sampleData.json'
   componentDidMount() {
     axios.get('sampleData.json', {
       params: {
-        // attributes: "country,location,populationDensity,humanDevelopmentIndex,gini,gdpPerCapita,medianAge,aged65Older,aged70Older,extremePovertyRate,cardiovascDeathRate,diabetesPrevalence,femaleSmokers,maleSmokers,hospitalBedsPerThousand,lifeExpectancy,handwashingFacilities"
-        attributes: "country,location,populationDensity,humanDevelopmentIndex,gini"
+        attributes: "country,location,populationDensity,humanDevelopmentIndex,gini,gdpPerCapita,medianAge,aged65Older,aged70Older,extremePovertyRate,cardiovascDeathRate,diabetesPrevalence,femaleSmokers,maleSmokers,hospitalBedsPerThousand,lifeExpectancy,handwashingFacilities"
+        // attributes: "country,location,populationDensity,humanDevelopmentIndex,gini"
       }
     })
     .then(res => {
@@ -54,112 +53,79 @@ export default class Risks extends Component {
 
 
   render() {
-    // const columns = [
-    //   {
-    //     title: "Country",
-    //     dataIndex: "country",
-    //     key: "country",
-    //     render: (country) => (
-    //     <a href={`/countries/${country.codes.alpha3Code}`}>
-    //       {country.name}
-    //     </a>
-    //     // <LinkContainer to={`/countries/${country.codes.alpha3Code}`}>
-    //     //   <a>{country.name}</a>
-    //     // </LinkContainer>
-    //     ),
-    //     sorter: (a, b) => a.country.name.localeCompare(b.country.name),
-    //   },
-    //   {
-    //     title: "Life Expectancy",
-    //     dataIndex: "lifeExpectancy",
-    //     key: "lifeExpectancy",
-    //     sorter: (a, b) => a.lifeExpectancy - b.lifeExpectancy,
-    //   },
-    //   {
-    //     title: "Human Development Index",
-    //     dataIndex: "humanDevelopmentIndex",
-    //     key: "humanDevelopmentIndex",
-    //     sorter: (a, b) => a.humanDevelopmentIndex - b.humanDevelopmentIndex,
-    //   },
-    //   {
-    //     title: "Population Density",
-    //     dataIndex: "populationDensity",
-    //     key: "populationDensity",
-    //     render: (population) => <>{population.toLocaleString()}</>,
-    //     sorter: (a, b) => a.populationDensity - b.populationDensity,
-    //   },
-    //   {
-    //     title: "Gini",
-    //     dataIndex: "gini",
-    //     key: "gini",
-    //     sorter: (a, b) => a.gini - b.gini,
-    //   },
-    //   {
-    //     title: "Explore Risks",
-    //     dataIndex: "exploreRisk",
-    //     key: "exploreRisk",
-    //     render: (code) => (
-    //       // <LinkContainer to={`/risks/${code}`}>
-    //       //   <Button>Explore</Button>
-    //       // </LinkContainer>
-
-    //       <a href={`/risks/${code}`}>
-    //         <Button>Explore</Button>
-    //       </a>
-    //     ),
-    //   },
-    //   {
-    //     title: "Explore Cases",
-    //     dataIndex: "exploreRisk",
-    //     key: "exploreCase",
-    //     render: (code) => (
-    //       // <LinkContainer to={`/cases/${code}`}>
-    //       //   <Button>Explore</Button>
-    //       // </LinkContainer>
-    //       <a href={`/cases/${code}`}>
-    //         <Button>Explore</Button>
-    //       </a>
-    //     ),
-    //   },
-    // ];
-
-    // const data = [
-    //   {
-    //     key: "1",
-    //     country: GBRData.country,
-    //     lifeExpectancy: GBRData.lifeExpectancy,
-    //     humanDevelopmentIndex: GBRData.humanDevelopmentIndex,
-    //     populationDensity: GBRData.populationDensity,
-    //     gini: GBRData.gini,
-    //     exploreRisk: GBRData.country.codes.alpha3Code,
-    //   },
-    //   {
-    //     key: "2",
-    //     country: MEXData.country,
-    //     lifeExpectancy: MEXData.lifeExpectancy,
-    //     humanDevelopmentIndex: MEXData.humanDevelopmentIndex,
-    //     populationDensity: MEXData.populationDensity,
-    //     gini: MEXData.gini,
-    //     exploreRisk: MEXData.country.codes.alpha3Code,
-    //   },
-    //   {
-    //     key: "3",
-    //     country: USAData.country,
-    //     lifeExpectancy: USAData.lifeExpectancy,
-    //     humanDevelopmentIndex: USAData.humanDevelopmentIndex,
-    //     populationDensity: USAData.populationDensity,
-    //     gini: USAData.gini,
-    //     exploreRisk: USAData.country.codes.alpha3Code,
-    //   },
-    // ];
+    const columns = [
+      {
+        title: "Country",
+        dataIndex: "country",
+        key: "country",
+        render: (country) => (
+        <a href={`/countries/${country.codes.alpha3Code}`}>
+          {country.name}
+        </a>
+        ),
+        sorter: (a, b) => a.country.name.localeCompare(b.country.name),
+      },
+      {
+        title: "Life Expectancy",
+        dataIndex: "lifeExpectancy",
+        key: "lifeExpectancy",
+        sorter: (a, b) => a.lifeExpectancy - b.lifeExpectancy,
+      },
+      {
+        title: "Human Development Index",
+        dataIndex: "humanDevelopmentIndex",
+        key: "humanDevelopmentIndex",
+        sorter: (a, b) => a.humanDevelopmentIndex - b.humanDevelopmentIndex,
+      },
+      {
+        title: "Population Density",
+        dataIndex: "populationDensity",
+        key: "populationDensity",
+        render: (population) => <>{population.toLocaleString()}</>,
+        sorter: (a, b) => a.populationDensity - b.populationDensity,
+      },
+      {
+        title: "Gini",
+        dataIndex: "gini",
+        key: "gini",
+        sorter: (a, b) => a.gini - b.gini,
+      },
+      {
+        title: "Explore Risks",
+        dataIndex: "country",
+        key: "country",
+        render: (country) => (
+          <a href={`/risks/${country.codes.alpha3Code}`}>
+            <Button>Explore</Button>
+          </a>
+        )
+      },
+      {
+        title: "Explore Cases",
+        dataIndex: "country",
+        key: "country",
+        render: (country) => (
+          <a href={`/cases/${country.codes.alpha3Code}`}>
+            <Button>Explore</Button>
+          </a>
+        ),
+      },
+    ];
 
     const data = this.state.riskData
+    // var riskInstance = {country, location, populationDensity, humanDevelopmentIndex, gini, gdpPerCapita, medianAge, aged65Older, aged70Older, extremePovertyRate, cardiovascDeathRate, diabetesPrevalence, femaleSmokers, maleSmokers, hospitalBedsPerThousand, lifeExpectancy, handwashingFacilities};
 
-    // get all country cards in the current view
+    // get all risk entries in the current view
     const currentView = data && data.length > 0 && data
       .slice(this.state.firstEntryIndex, this.state.lastEntryIndex)
-      .map(riskData => <RiskEntry key={riskData.country.codes.alpha3Code} data={riskData}/>)
-    console.log(this.state.firstEntryIndex, this.state.lastEntryIndex)
+      .map(riskData => <Table columns={columns} dataSource={data} onChange={this.handleChange} />);
+      // .map(riskData => <div key={riskData.country}>| {riskData.populationDensity} | {riskData.gini}| </div>);
+
+
+    
+
+
+    console.log(this.state.firstEntryIndex, this.state.lastEntryIndex);
     const risks = data 
       ? (<Fragment>
         {currentView}
@@ -173,34 +139,41 @@ export default class Risks extends Component {
       : <div/>;
 
     return (
-      // <div className="App">
-      //   {/* <header className="App-header"> */}
-      //   <h1
-      //     style={{
-      //       fontWeight: "800",
-      //       fontSize: "2em",
-      //       marginTop: "20px",
-      //       marginBottom: "20px",
-      //     }}
-      //   >
-      //     Risk Factors & Statistics{" "}
-      //   </h1>
-      //   <Table
-      //     style={{ margin: "0 5vw", outline: "1px solid lightgrey" }}
-      //     columns={columns}
-      //     dataSource={data}
-      //     pagination={false}
-      //   />
-      //   {/* </header> */}
-      // </div>
-
-
-    <div className="App">
-      <h1 style={{ fontWeight: '800', fontSize: '2em', marginTop: '20px', marginBottom: '20px' }}>Risks </h1>
-      <div style={{ margin: "0 5vw" }}>
-        {risks}
+      <div className="App">
+        <h1
+          style={{
+            fontWeight: "800",
+            fontSize: "2em",
+            marginTop: "20px",
+            marginBottom: "20px",
+          }}
+        >
+          Risk Factors & Statistics{" "}
+        </h1>
+        <Table style={{ margin: "0 5vw", outline: "1px solid lightgrey" }} columns={columns} dataSource={data} onChange={this.handleChange}></Table>
       </div>
-    </div>
+      // currentView
+    // <div className="App">
+    //   <h1 style={{ fontWeight: '800', fontSize: '2em', marginTop: '20px', marginBottom: '20px' }}>Risks </h1>
+    //   {/* <div style={{ margin: "0 5vw" }}>
+    //     before {risks} after
+    //   </div> */}
+    //   {/* <Table
+    //     style={{ margin: "0 5vw", outline: "1px solid lightgrey" }}
+    //     // columns={columns}
+    //     dataSource={<RiskEntry key={data.country.codes.alpha3Code}></RiskEntry>}
+    //     pagination={false}
+    //   /> */}
+    //   <div>
+    //     data: {data[0]}
+    //     here: {currentView[0]}
+    //     {/* {RiskEntry.country} */}
+    //     {/* {this.riskData} */}
+    //     {/* const {country, location, populationDensity, humanDevelopmentIndex, gini, gdpPerCapita, medianAge, aged65Older, aged70Older, extremePovertyRate, cardiovascDeathRate, diabetesPrevalence, femaleSmokers, maleSmokers, hospitalBedsPerThousand, lifeExpectancy, handwashingFacilities} = this.props.data; */}
+    //     {/* for every element in currentView, put in table */}
+    //     {/* {risks} */}
+    //   </div>
+    // </div>
     );
   }
 }

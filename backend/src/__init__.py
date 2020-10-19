f0b79ece10fdb0b241b90645f87780f2c06a2fc8
Cameron Doggett <camd.99@gmail.com>
2020-10-19 13:12:37 -0500

    Added CORS and database url now based on environment variable

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,14 +1,24 @@
 """
 Sets up the Flask application and the database connection.
 """
+import os
+from pathlib import Path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
-from . import secret
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+# try to load from .env file if DATABASE_URL not defined already
+if os.getenv("DATABASE_URL") is None:
+    # .env file expected to be in /backend directory
+    env_path = Path(os.path.dirname(os.path.realpath(__file__))) / "../.env"
+    load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__, static_folder="../static")
-app.config["SQLALCHEMY_DATABASE_URI"] = secret.DATABASE_URL
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["RESTFUL_JSON"] = {"ensure_ascii": False}
+CORS(app)
 db = SQLAlchemy(app)
 api = Api(app)

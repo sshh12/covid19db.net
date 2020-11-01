ebf50f87470c5fc6d5c40fd93fe37b1bfe2f6467
Yash Kakodkar <33426213+YashKakodkar@users.noreply.github.com>
2020-11-01 01:51:41 -0500

    Refactored main page components

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Map from "../components/map";
 import axios from "../client";
+import {TotalStats} from './../components/mainComponents';
+import './../styling/main.css';
 
 export default class Main extends Component {
   constructor() {
@@ -31,24 +33,15 @@ export default class Main extends Component {
     }
 
     return (
-      <div className="App" style={{ display: "flex", flexDirection: "row" }}>
-        <div style={{ flex: 1 }}>
-          <h1>Global Statistics</h1>
-          <div className="totals">
-            <h2 className="totals-title">Total Cases</h2>
-            <h2 className="totals-data">{data.totals.cases}</h2>
-          </div>
-          <div className="totals">
-            <h2 className="totals-title">Total Deaths</h2>
-            <h2 className="totals-data">{data.totals.deaths}</h2>
-          </div>
-          <div className="totals">
-            <h2 className="totals-title">Total Recovered</h2>
-            <h2 className="totals-data">{data.totals.recovered}</h2>
-          </div>
-          <div className="totals">
-            <h2 className="totals-title">Total Active</h2>
-            <h2 className="totals-data">{data.totals.active}</h2>
+      <div className="App" style={{ display: "flex", flexDirection: "column"}}>
+        <h1 className="global-totals-title">Coronavirus is bad. Cases are rising. We're here to organize data. Please replace this crappy text.</h1>
+        <div style={{ flex: 1, alignContent:'center'}}>
+          <h1 className="global-totals-title">Global Statistics</h1>
+          <div style={{ display: "flex", flexDirection: "row", marginLeft: 50}}>
+            <TotalStats title="Total Cases" data={data.totals.cases}/>
+            <TotalStats title="Total Deaths" data={data.totals.deaths}/>
+            <TotalStats title="Total Recovered" data={data.totals.recovered}/>
+            <TotalStats title="Total Active" data={data.totals.active}/>
           </div>
         </div>
         <div style={{ flex: 3 }}>
@@ -63,3 +56,6 @@ export default class Main extends Component {
     );
   }
 }
+
+
+let style 

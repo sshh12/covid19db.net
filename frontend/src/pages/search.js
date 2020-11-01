5f899688a0d52c458a9f1d9ee1e29ebb17462b15
Yash Kakodkar <33426213+YashKakodkar@users.noreply.github.com>
2020-11-01 11:45:47 -0600

    Restyled landing page temporarily

---
+++ frontend/src/pages/search.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Form, Button, Input } from 'antd';
+import './../styling/main.css';
+
+export default class Search extends Component {
+  render() { 
+
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import { About, Cases, Countries, Main, Risks, GlobalNews } from './pages/allPages'
+import { About, Cases, Countries, Main, Risks, GlobalNews, Search } from './pages/allPages'
 import RiskInstance from './components/risks/riskInstance';
 import CountryInstance from "./components/country/countryInstance";
 import CaseInstance from "./components/caseInstances/caseInstance";
@@ -28,6 +28,7 @@ function Routes() {
         return <RiskInstance code={countryCode} />
       }}></Route>
       <Route exact path="/globalNews"><GlobalNews /></Route>
+      <Route exact path="/search"><Search /></Route>
     </Switch>
   );
 }

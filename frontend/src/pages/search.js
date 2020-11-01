739cdb1dea8f898fcea8682b174e0c0f67330dff
Yash Kakodkar <33426213+YashKakodkar@users.noreply.github.com>
2020-11-01 11:46:37 -0600

    Moved search to its own page

--- frontend/src/pages/search.js
+++ frontend/src/pages/search.js
@@ -7,7 +7,15 @@ export default class Search extends Component {
 
     return (
       <div className="App">
-        
+        <div style={{backgroundColor:"#323776", paddingBottom: 15}}>
+        <h1 style={{paddingTop:20, paddingBottom: 20, fontSize:'2em'}} className="main-text">Search</h1>
+        <Form style={{display: 'block'}}>
+          <Form.Item style={{display: 'inline-block', width: "25%"}}>
+            <Input placeholder="Search bar not ready yet ... :(" />
+            <Button style={{backgroundColor:"#ededed", color: "#323776", fontWeight:"bold", margin:15}}>Search</Button>
+          </Form.Item>
+        </Form>
+        </div>
       </div>
     );
   }

d69027f01afb93a0202620196801b40de6919fc5
Yash Kakodkar <33426213+YashKakodkar@users.noreply.github.com>
2020-11-01 01:51:41 -0500

    Refactored main page components

---
+++ frontend/src/components/mainComponents.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../styling/main.css';
+
+function TotalStats(props) {
+  return <div className='totals'>
+    <h2 className='totals-title'>{props.title}</h2>
+    <h2 className='totals-data'>{props.data}</h2>
+  </div>
+}
+
+export {
+  TotalStats
+}

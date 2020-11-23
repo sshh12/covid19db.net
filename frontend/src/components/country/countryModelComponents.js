0b95da16bf1a1a7a2de60c7702e193db8203c75a
Jeffrey Moulckers <jrmoulckers@gmail.com>
2020-11-22 19:04:49 -0600

    added comparable countries

--- frontend/src/components/country/countryModelComponents.js
+++ frontend/src/components/country/countryModelComponents.js
@@ -2,6 +2,7 @@ import CountryCardView from "./modelComponents/countryCardView";
 import CountryGridControl from "./modelComponents/countryGridControl";
 import CountryPagination from "./modelComponents/countryPagination";
 import CountrySortSelection from "./modelComponents/countrySortSelection";
+import CountryComparison from "./countryComparison";
 import SORT_TYPES from "./modelComponents/countrySortTypes";
 
 export {
@@ -9,5 +10,6 @@ export {
   CountryGridControl,
   CountryPagination,
   CountrySortSelection,
+  CountryComparison,
   SORT_TYPES,
 };

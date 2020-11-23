3c7c2d34e7bc82493a1463e6400fb71feaec9924
Jeffrey Moulckers <jrmoulckers@gmail.com>
2020-11-22 21:41:53 -0600

    added loading spinner to home page

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import InfectionsMap from "../components/infectionsMap";
 import axios from "../client";
 import TotalStats from "./../components/mainComponents";
-import { Spinner } from "react-bootstrap";
+import { Skeleton, Spin } from "antd";
 import "./../styling/main.css";
 
 export default function Main() {
@@ -20,15 +20,10 @@ export default function Main() {
       })
       .then((res) => setCases(res.data));
   }, []);
-  if (!data || !cases) {
-    return (
-      <div className="App" style={{ padding: "30px" }}>
-        <Spinner animation="border" variant="primary" />
-      </div>
-    );
-  }
-  return (
-    <div className="App" style={{ display: "flex", flexDirection: "column" }}>
+  const globalStats = !data ? (
+    <Skeleton active />
+  ) : (
+    <Fragment>
       <h1 style={{ paddingTop: 20, paddingBottom: 40 }} className="main-text">
         Coronavirus sucks. Maybe this can help :)
       </h1>
@@ -59,15 +54,26 @@ export default function Main() {
           data={data.totals.active?.toLocaleString()}
         />
       </div>
-      <div style={{ flex: 3 }}>
-        <InfectionsMap
-          caseStats={cases}
-          center={[15, 30]}
-          zoom={1.95}
-          height={window.innerHeight - 79 + (420 - 420)}
-          width={"100vw"}
-        />
-      </div>
+    </Fragment>
+  );
+  const caseMap = !cases ? (
+    <Spin size="large" />
+  ) : (
+    <div style={{ flex: 3 }}>
+      <InfectionsMap
+        caseStats={cases}
+        center={[15, 30]}
+        zoom={1.95}
+        height={window.innerHeight - 79 + (420 - 420)}
+        width={"100vw"}
+      />
+    </div>
+  );
+
+  return (
+    <div className="App" style={{ display: "flex", flexDirection: "column" }}>
+      {globalStats}
+      {caseMap}
     </div>
   );
 }

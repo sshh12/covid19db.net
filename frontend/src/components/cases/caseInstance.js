f65e45cb8687478b946d2afacc614d6f17103fa9
Yash Kakodkar <33426213+YashKakodkar@users.noreply.github.com>
2020-11-24 14:46:40 -0600

    Case stats render programitcally

--- frontend/src/components/cases/caseInstance.js
+++ frontend/src/components/cases/caseInstance.js
@@ -5,6 +5,7 @@ import { Button } from "antd";
 import Map from "../../components/map";
 import axios from "../../client";
 import "../../styling/caseInstance.css";
+import caseStaticData from "./caseInstanceStaticData";
 
 class CaseInstance extends Component {
   constructor() {
@@ -61,6 +62,7 @@ class CaseInstance extends Component {
 
   render() {
     //Need to make flex box items responsive when changing window size
+    let { totalData, recentData, yesterdayData, generalData } = this.state;
     const data = this.state.caseData;
     if (!data) {
       return <div />;
@@ -91,31 +93,15 @@ class CaseInstance extends Component {
           justifyContent: "start",
         }}
       >
-        <Totals
-          title="Total Cases"
-          data={data.totals.cases}
-          description="Total number of confirmed cases"
-        />
-        <Totals
-          title="Total Active"
-          data={data.totals.active}
-          description="Total number of active cases"
-        />
-        <Totals
-          title="Total Deaths"
-          data={data.totals.deaths}
-          description="Total number of deaths"
-        />
-        <Totals
-          title="Total Recovered"
-          data={data.totals.recovered}
-          description="Total number of recoveries"
-        />
-        <Totals
-          title="Total Tests"
-          data={data.testing.totalTests.value}
-          description="Total number of tests"
-        />
+        {[...Array(5).keys()].map((i) => {
+          return (
+            <Totals
+              title={caseStaticData.totalStatsTitles[i]}
+              data={totalData[i]}
+              description={caseStaticData.totalStatsDescriptions[i]}
+            />
+          );
+        })}
       </div>
     );
 
@@ -125,61 +111,19 @@ class CaseInstance extends Component {
           <h2 id="subtitle">Recent Statistics</h2>
           <h2 id="new-stats-date">{data.date}</h2>
         </div>
-        <div
-          style={{
-            marginTop: "5px",
-            display: "flex",
-            justifyContent: "left",
-            flexWrap: "wrap",
-          }}
-        >
-          <NewStats
-            title="Cases"
-            data={data.new.cases}
-            yesterday={
-              Math.round((data.derivativeNew.cases + Number.EPSILON) * 100) /
-              100
-            }
-            description="Number of new confirmed cases"
-          />
-          <NewStats
-            title="Active"
-            data={data.new.active}
-            yesterday={
-              Math.round((data.derivativeNew.active + Number.EPSILON) * 100) /
-              100
-            }
-            description="Number of new active cases"
-          />
-          <NewStats
-            title="Deaths"
-            data={data.new.deaths}
-            yesterday={
-              Math.round((data.derivativeNew.deaths + Number.EPSILON) * 100) /
-              100
-            }
-            description="Number of new deaths"
-          />
-          <NewStats
-            title="Recovered"
-            data={data.new.recovered}
-            yesterday={
-              Math.round(
-                (data.derivativeNew.recovered + Number.EPSILON) * 100
-              ) / 100
-            }
-            description="Number of new recoveries"
-          />
-          <NewStats
-            title="Tests"
-            data={data.testing.newTests.value}
-            yesterday={
-              Math.round(
-                (data.testing.newTestsSmoothed.value + Number.EPSILON) * 100
-              ) / 100
-            }
-            description="Number of new tests"
-          />
+        <div className="card-stats-div">
+          {[...Array(5).keys()].map((i) => {
+            return (
+              <NewStats
+                title={caseStaticData.newStatsTitles[i]}
+                data={recentData[i]}
+                yesterday={
+                  Math.round((yesterdayData[i] + Number.EPSILON) * 100) / 100
+                }
+                description={caseStaticData.newStatsDescriptions[i]}
+              />
+            );
+          })}
         </div>
       </div>
     );
@@ -187,55 +131,16 @@ class CaseInstance extends Component {
     const generalStats = (
       <div style={{ marginTop: "50px" }}>
         <h2 id="subtitle">General Statistics</h2>
-        <div
-          style={{
-            marginTop: "5px",
-            display: "flex",
-            justifyContent: "left",
-            flexWrap: "wrap",
-          }}
-        >
-          <GenStats
-            title="Fatality Rate"
-            data={
-              Math.round((data.percentages.fatality + Number.EPSILON) * 100) /
-              100
-            }
-            description="Total deaths/total cases * 100"
-          />
-          <GenStats
-            title="Infection Rate"
-            data={
-              Math.round((data.percentages.infected + Number.EPSILON) * 100) /
-              100
-            }
-            description="Total cases/total population * 100"
-          />
-          <GenStats
-            title="Positivity Rate"
-            data={
-              Math.round(
-                (data.testing.positiveRate.value + Number.EPSILON) * 100
-              ) / 100
-            }
-            description="Positive tests/total tests * 100"
-          />
-          <GenStats
-            title="Recovery Percentage"
-            data={
-              Math.round(
-                (data.percentages.haveRecovered + Number.EPSILON) * 100
-              ) / 100
-            }
-            description="Total recovered/total cases * 100"
-          />
-          <GenStats
-            title="Active Rate"
-            data={
-              Math.round((data.percentages.active + Number.EPSILON) * 100) / 100
-            }
-            description="Total active/total cases * 100"
-          />
+        <div className="card-stats-div">
+          {[...Array(5).keys()].map((i) => {
+            return (
+              <GenStats
+                title={caseStaticData.genStatsTitles[i]}
+                data={Math.round((generalData[i] + Number.EPSILON) * 100) / 100}
+                description={caseStaticData.genStatsDescriptions[i]}
+              />
+            );
+          })}
         </div>
       </div>
     );

c58ed54d7cc1c9393a24e8843f0b54c64d572f77
Vassi Gianitsos <vgianitsos@yahoo.com>
2020-11-23 21:44:16 -0600

    made return statement shorter

--- frontend/src/pages/vis.js
+++ frontend/src/pages/vis.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import VisualizationA from "./../components/vis/visA";
 import VisualizationB from "./../components/vis/visB";
 import VisualizationC from "./../components/vis/visC";
@@ -8,45 +8,8 @@ import ProviderVisualizationC from "./../components/vis/provVisC";
 import { Card, Col, Row } from "antd";
 
 export default function Visualizations() {
-  return (
-    <div className="App">
-      <h1
-        style={{
-          fontWeight: "800",
-          fontSize: "2em",
-          marginTop: "20px",
-          marginBottom: "20px",
-        }}
-      >
-        Visualizations
-      </h1>
-      <Row justify="center">
-        <Col>
-          <Card hoverable style={{ height: "520px", width: "600px" }}>
-            <h4>Cases vs Population (Log Scale)</h4>
-            <div style={{ height: "440px", width: "560px" }}>
-              <VisualizationA />
-            </div>
-          </Card>
-        </Col>
-        <Col>
-          <Card hoverable style={{ height: "520px", width: "600px" }}>
-            <h4>Deaths by Country (Top 20)</h4>
-            <div style={{ height: "440px", width: "560px" }}>
-              <VisualizationB />
-            </div>
-          </Card>
-        </Col>
-        <Col>
-          <Card hoverable style={{ height: "520px", width: "600px" }}>
-            <h4>Share of Global Cases</h4>
-            <div style={{ height: "440px", width: "560px" }}>
-              <VisualizationC />
-            </div>
-          </Card>
-        </Col>
-      </Row>
-      <hr />
+  const providerVis = (
+    <Fragment>
       <div style={{ textAlign: "center"}}>
         <h1
           style={{
@@ -85,6 +48,56 @@ export default function Visualizations() {
           </Card>
         </Col>
       </Row>
+    </Fragment>
+  )
+
+  const myVis = (
+    <Fragment>
+      <Row justify="center">
+        <Col>
+          <Card hoverable style={{ height: "520px", width: "600px" }}>
+            <h4>Cases vs Population (Log Scale)</h4>
+            <div style={{ height: "440px", width: "560px" }}>
+              <VisualizationA />
+            </div>
+          </Card>
+        </Col>
+        <Col>
+          <Card hoverable style={{ height: "520px", width: "600px" }}>
+            <h4>Deaths by Country (Top 20)</h4>
+            <div style={{ height: "440px", width: "560px" }}>
+              <VisualizationB />
+            </div>
+          </Card>
+        </Col>
+        <Col>
+          <Card hoverable style={{ height: "520px", width: "600px" }}>
+            <h4>Share of Global Cases</h4>
+            <div style={{ height: "440px", width: "560px" }}>
+              <VisualizationC />
+            </div>
+          </Card>
+        </Col>
+      </Row>
+
+    </Fragment>
+  )
+  return (
+    <div className="App">
+      <h1
+        style={{
+          fontWeight: "800",
+          fontSize: "2em",
+          marginTop: "20px",
+          marginBottom: "20px",
+        }}
+      >
+        Visualizations
+      </h1>
+        {myVis}
+      <hr />
+      {providerVis}
+      
     </div>
   );
 }
